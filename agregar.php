456f640a37728c90e22463591077eac8fc9c6c02
Agustin-Ramirez <diego.ramirez@globant.com>
2021-06-04 17:08:08 -0300

    delate-mail

--- agregar.php
+++ agregar.php
@@ -20,11 +20,6 @@ include('DB.php');
 include('peliculas.php');
     $name = $_POST["nombre"];
     $img = $_POST["img"];
-    $email = "diego.ramirez@anima.edu.uy";
-    $header = "From: noreply@example.com" . "\r\n";
-    $header.= "Reply-to: noreply@example.com" . "\r\n";
-    $header.= "X-Mailer: PHP/" . phpversion();
-    $mail = @mail($email, $name, $img, $header);
     $insertar = "INSERT INTO peliculas(nombre, img) VALUES ('$name', '$img')";
     $resultado = mysqli_query($conn, $insertar);
 ?>
